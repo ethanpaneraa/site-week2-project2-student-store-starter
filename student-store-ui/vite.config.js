a16691dc2dd6cd04c8e67fe09487af38dbcb1b8a
Ethan Pineda <joshuapineda66@gmail.com>
2023-06-16 13:04:14 -0700

    fixing base url

--- student-store-ui/vite.config.js
+++ student-store-ui/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig((envConfig) => ({
-  base:"/site-week2-project2-student-store-starter", 
+  base:"/site-week2-project2-student-store-starter/", 
   plugins: [react()],
   server: {
     open: true,
