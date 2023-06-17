6145c3e35da9da078ceb4654581e75ca79ff4405
Ethan Pineda <joshuapineda66@gmail.com>
2023-06-16 22:28:04 -0700

    about us and contact us done

--- student-store-ui/src/components/About/About.jsx
+++ student-store-ui/src/components/About/About.jsx
@@ -5,11 +5,23 @@ const About = () => {
 
     return (
         <div id="about" className="about">
-            <p>The codepath student store offers great products at great prices from a great team and for a great cause.
-
-We've searched far and wide for items that perk the interests of even the most eccentric students and decided to offer them all here in one place.
-
-All proceeds go towards bringing high quality CS education to college students around the country.</p>
+            <div className="about-us-container">
+                <h2>About Us</h2>
+                    <div className="summary">
+                        <div className="text2">
+                            <p>The codepath student store offers great products at great prices from a great team and for a great cause.
+                            <br />
+                            <br /> 
+                            We've searched far and wide for items that perk the interests of even the most eccentric students and decided to offer them all here in one place.
+                            <br /> 
+                            <br /> 
+                            All proceeds go towards bringing high quality CS education to college students around the country.</p>
+                        </div>
+                        {/* <div className="about-us-image">
+                            <img src="https://codepath-student-store-demo.surge.sh/assets/giant_codepath.6952ef57.svg" alt="CodePath Image Logo" />
+                        </div> */}
+                    </div>
+            </div>
         </div>
     );
 
--- student-store-ui/src/components/Contact/Contact.jsx
+++ student-store-ui/src/components/Contact/Contact.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import "./Contact.css"; 
-
+import SocialMediaLinks from "../SocialMediaLinks/SocialMediaLinks"
 const Contact = () => {
 
     return (
         <div id="contact" className="Contact">
-            <p>Email: exmapleEmail@gmail.com</p>
-            <p>Phone: 999-999-9999</p>
-            <p>Address: 415 Mission Street</p>
+            <div className="contact-container">
+                <h2>Contact Us</h2>
+                <div className="contact-summary">
+                    <ul className="contact-info-list">
+                        <li>
+                            <span className="label">Email: </span>
+                            <span>code@path.org</span>
+                        </li>
+                        <li>
+                            <span className="label">Phone: </span>
+                            <span>1-800-CODEPATH</span>
+                        </li>
+                        <li>
+                            <span className="label">Address: </span>
+                            <span>415 Mission Street</span>
+                        </li>
+                        <li>
+                            <span><SocialMediaLinks /> </span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="contact-us-media">
+                    <img src="https://codepath-student-store-demo.surge.sh/assets/happy_person.517b658d.svg" alt="picture of a women smiling" />
+                </div>
+            </div>
         </div>
     );
 
